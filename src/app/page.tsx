d8214c2f9348f7ffbad051691e2167d4e268ba7d
Shaurya Srivastava <shaurya1606@users.noreply.github.com>
2025-08-26 14:19:29 +0530

    added the Why Choose Us Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/HeroSection'
 import FeaturedSection from '@/components/FeaturedSection'
+import WhyChooseUsSection from '@/components/WhyChooseUsSection'
 
 export default function Home() {
   return (
     <main className="antialiased bg-grid-white/[0.02] bg-black/[0.96] min-h-screen">
       <HeroSection />
       <FeaturedSection />
+      <WhyChooseUsSection />
     </main>
   );
 }
