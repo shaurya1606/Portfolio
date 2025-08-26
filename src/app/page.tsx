91eb6f4cf215626ffa7fac21b733086b48258808
Shaurya Srivastava <shaurya1606@users.noreply.github.com>
2025-08-26 13:47:58 +0530

    added the featured section with cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import HeroSection from '@/components/HeroSection'
+import FeaturedSection from '@/components/FeaturedSection'
+
 export default function Home() {
   return (
     <main className="antialiased bg-grid-white/[0.02] bg-black/[0.96] min-h-screen">
       <HeroSection />
-    </main> 
+      <FeaturedSection />
+    </main>
   );
 }
